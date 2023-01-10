1c1adfb5807a373a5eb71a707a26f930de30109c
Joan Barrull Ribalta <giria@users.noreply.github.com>
2023-01-10 14:14:18 +0000

    before transitions

---
+++ Landmarks/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//Created for Landmarks in 2023
+// Using Swift 5.0
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
